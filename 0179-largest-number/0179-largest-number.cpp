5dd6fc8dd76cc022db21add885263eaa113589e8
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-06-26 18:08:50 +0530

    Time: 3 ms (75.18%), Space: 15.1 MB (96.57%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -9,16 +9,17 @@ public:
             return (f+s) > (s+f) ;
         });
         int i=0;
-        if(nums[0] == 0){
-            while( i<nums.size() && nums[i] == 0){
-                i++;
-            }
-            if(i == nums.size())
-                return "0";
-        }
+        // if(nums[0] == 0){
+        //     while( i<nums.size() && nums[i] == 0){
+        //         i++;
+        //     }
+        //     if(i == nums.size())
+        //         return "0";
+        // }
         for(int j = i; j<nums.size(); j++){
             ans += to_string(nums[j]);
         }
+        if(ans[0] == '0' ) return "0";
 
         return ans;
     }
