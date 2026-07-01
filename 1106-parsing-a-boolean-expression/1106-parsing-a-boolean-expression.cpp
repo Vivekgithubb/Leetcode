90965c26bffe39dd5b348ba8cfc95901710e1f5f
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-07-01 18:14:34 +0530

    Time: 0 ms (100%), Space: 10.8 MB (77.3%) - LeetHub

--- 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
@@ -1,64 +1,74 @@
 class Solution {
 public:
-    bool parser(vector<char>v, char op){
-        char val;
-        bool ans;
-        if(v.size() == 1){
-            bool curOp = v[0] =='f' ? 0 : 1;
+    // bool parser(vector<char>v, char op){
+    //     char val;
+    //     bool ans;
+    //     if(v.size() == 1){
+    //         bool curOp = v[0] =='f' ? 0 : 1;
 
-            if(op =='&' || op =='|') return curOp;
-            if(op =='!') return !curOp;
-        }
-        if(op == '&'){
-            ans = v[0] == 'f' ? 0 : 1;
-            for(int i=1;i<v.size();i++){
-                bool curVal = v[i] == 'f' ? 0 : 1;
-                ans &= curVal;
-            }
-        }
-        else if(op == '|'){
-            ans = v[0] == 'f' ? 0 : 1;
-            for(int i=1;i<v.size();i++){
-                bool curVal = v[i] == 'f' ? 0 : 1;
-                ans |= curVal;
-            }
-        }
-        else if(op == '!'){
-            bool curVal = v[0] == 'f' ? 0 : 1;
-            return  !curVal;
-        }
+    //         if(op =='&' || op =='|') return curOp;
+    //         if(op =='!') return !curOp;
+    //     }
+    //     if(op == '&'){
+    //         ans = v[0] == 'f' ? 0 : 1;
+    //         for(int i=1;i<v.size();i++){
+    //             bool curVal = v[i] == 'f' ? 0 : 1;
+    //             ans &= curVal;
+    //         }
+    //     }
+    //     else if(op == '|'){
+    //         ans = v[0] == 'f' ? 0 : 1;
+    //         for(int i=1;i<v.size();i++){
+    //             bool curVal = v[i] == 'f' ? 0 : 1;
+    //             ans |= curVal;
+    //         }
+    //     }
+    //     else if(op == '!'){
+    //         bool curVal = v[0] == 'f' ? 0 : 1;
+    //         return  !curVal;
+    //     }
 
-        return ans;
-    }
+    //     return ans;
+    // }
     bool parseBoolExpr(string s) {
         int n = s.size();
         if(n == 1) return s[0] == 'f' ? 0 : 1;
         stack<char>st;
         bool ans;
         for(int i=0;i<n;i++){
-            if(s[i] == ',' || s[i] == ' ') continue;
-
-            if(st.empty() || s[i] != ')'){
-                st.push(s[i]);
+            if(s[i] == ',' || s[i] == '(') 
                 continue;
-            }
 
-            if(s[i] == ')'){
-                vector<char>v;
-                while(st.top() != '('){
-                    v.push_back(st.top());
+            if(s[i] != ')'){
+                st.push(s[i]);
+            }
+            else if(s[i] == ')'){
+                // vector<char>v;
+                bool hasFalse = false;
+                bool hasTrue = false;
+
+                while(st.top() != '!' && st.top() !='|' && st.top() != '&'){
+                    char curVal = st.top();
+                    if(curVal == 'f') hasFalse = true;
+                    if(curVal == 't') hasTrue = true;
                     st.pop();
                 }
-                st.pop();
                 char curOp = st.top();
                 st.pop();
-                ans = parser(v,curOp);
-                if(ans == false)
-                    st.push('f');
+                if(curOp == '!')
+                    st.push(hasTrue ? 'f' : 't');
+                else if( curOp == '&')
+                    st.push(hasFalse ? 'f' : 't');
                 else
-                    st.push('t');
+                    st.push(hasTrue ? 't' : 'f');
+
+                // ans = parser(v,curOp);
+                // if(ans == false)
+                //     st.push('f');
+                // else
+                //     st.push('t');
             }
         }
-        return ans;
+        return st.top() == 't';
     }
 };
