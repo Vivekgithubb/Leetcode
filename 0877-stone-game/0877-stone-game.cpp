2bd2ee344b73cdb7aa873afb0ca0c76e9a696046
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-07-03 00:43:39 +0530

    Time: 20 ms (9.55%), Space: 19.7 MB (19.51%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -8,17 +8,33 @@ public:
         int leftMax = total - maxAns(piles , total-piles[i] , i+1,j,dp);
         int rightMax = total - maxAns(piles , total-piles[j] , i,j-1,dp);
 
-        return max(leftMax,rightMax);
+        return dp[i][j]=max(leftMax,rightMax);
 
     }
     bool stoneGame(vector<int>& piles) {
         int total = 0;
         int n = piles.size();
-        for(auto i : piles) total += 1;
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+        for(auto i : piles) total += i;
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
 
-        // int ans = maxAns(piles,total,0,n-1,dp);
-        // return ans > total - ans;
-        return true;
+        int ans = maxAns(piles,total,0,n-1,dp);
+        return ans > total - ans;
+
+        // for(int i=n-1; i>=0 ; i--){
+        //     for(int j=0;j<n;j++){
+        //         if(i==j){
+        //             dp[i][j] = piles[i];
+        //             continue;
+        //         }
+        //         if(i>j) continue;
+        //         int leftMax = piles[i] - dp[i+1][j];
+        //         int rightMax = piles[j] - dp[i][j-1]; 
+
+        //         dp[i][j] = max(leftMax,rightMax);
+        //     }
+        // }
+
+        // return dp[1][n-1] >= 0;
+        // return true;
     }
 };
