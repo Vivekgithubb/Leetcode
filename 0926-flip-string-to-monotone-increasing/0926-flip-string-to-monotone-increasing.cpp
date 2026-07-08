8fffc20a04077e1ff79746bbb01703269ecef224
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-07-08 22:43:13 +0530

    Time: 194 ms (27.19%), Space: 81.8 MB (29.15%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -31,8 +31,36 @@ public:
     }
     int minFlipsMonoIncr(string s) {
         int n = s.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        int ans1 = LR(s,0,0,dp);
-        return ans1;
+        vector<vector<int>>dp(n+1,vector<int>(2,-1));
+        // int ans1 = LR(s,0,0,dp);
+        dp[n][1] = 0;
+        dp[n][0] = 0;
+        for(int ind=n-1 ; ind>=0; ind-- ){
+            for(int prev = 0; prev <=1 ;prev++){
+                int ans1 = INT_MAX;
+                int ans2 = INT_MAX;      
+                if(prev == 0){
+                    //no flip or flip
+                    if(s[ind] == '0'){
+                        ans1 = dp[ind+1][0]; 
+                        ans2 = 1 + dp[ind+1][1]; 
+                    }
+                    if( s[ind] == '1'){
+                        ans1 = dp[ind+1][1];
+                        ans2 = 1 + dp[ind+1][0];
+                    }
+                }else{
+                    //no flip or flip
+                    if(s[ind] == '0'){
+                        ans1 = 1 + dp[ind+1][1];
+                    }
+                    if( s[ind] == '1'){
+                        ans1 = dp[ind+1][1];
+                    }
+                }
+                dp[ind][prev] = min(ans1,ans2);
+            }
+        }
+        return dp[0][0];
     }
 };
