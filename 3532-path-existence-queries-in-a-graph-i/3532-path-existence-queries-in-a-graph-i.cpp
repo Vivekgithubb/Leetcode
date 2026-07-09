092ca0a04b780824afd2fad4bd13b26593172d6d
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-07-09 23:34:46 +0530

    Time: 74 ms (20%), Space: 235.9 MB (24.93%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -2,47 +2,21 @@ class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
         vector<bool>ans;
-        vector<vector<int>>q;
-        for(int i=0;i<n;i++){
-            if(  i == n-1 ){
-                q.push_back({i,i});
-                continue;
-            }
-            int k = i;
-            int j = i+1;
-            while(j<n && ((nums[j] - nums[k]) <= maxDiff)){
-                j++;
-                k++;
-            }
-            vector<int>temp = {i,k};
-            q.push_back(temp);
-            i = j-1;
+        vector<int>group(n,0);
+        int curGroup = 0;
+        for(int i=1;i<n;i++){
+            if(nums[i]-nums[i-1] > maxDiff)
+                curGroup++;
+            group[i] = curGroup;
         }
 
+        for(auto q : queries){
+            int u = q[0];
+            int v = q[1];
 
-        vector<vector<int>>range(n,vector<int>(2,-1));
-        int i=0;
-        for(int k=0; k < n ;k++){ 
-            int start = q[i][0];
-            int end = q[i][1];
-            if( k < start)
-                range[k] = { k , k};
-            else if( k <= end )
-                range[k] = { start , end};
-            else{
-                range[k] = {k,k};
-                i++;
-            }
-        }
-
-        for(auto x : queries){
-            int curS = x[0] >= x[1] ? x[1] : x[0];
-            int curE = x[0] >= x[1] ? x[0] : x[1];
-            int s = range[curE][0];
-            int e = range[curE][1];
-            if( curS >= s && curE <= e)
+            if( group[u] == group[v])
                 ans.push_back(true);
-            else 
+            else
                 ans.push_back(false);
         }
         return ans;
