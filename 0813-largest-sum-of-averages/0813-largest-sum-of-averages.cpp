6fd318dccb49ac426eb8b2e0b019092f94aea0c6
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-07-11 01:15:38 +0530

    Time: 4 ms (61.26%), Space: 11 MB (80.09%) - LeetHub

--- 0813-largest-sum-of-averages/0813-largest-sum-of-averages.cpp
+++ 0813-largest-sum-of-averages/0813-largest-sum-of-averages.cpp
@@ -1,33 +1,5 @@
 class Solution {
 public:
-    double solve(vector<int>&arr,int k , int i,vector<vector<double>>& dp){
-        int n = arr.size();
-        if( i == n) return -1e9;
-        if( k == 1 && i < n){
-            double curAvg = arr[i];
-            for(int j=i+1; j<n ;j++){
-                curAvg += (double)arr[j];
-            }
-            curAvg = curAvg/(n-i);
-            return curAvg;
-        }
-
-        k--;
-        if(dp[i][k] != -1) return dp[i][k];
-        
-        double curAvg = arr[i];
-        double sum = 0;
-        double maxSum = -1e9;
-
-        for(int j=i;j<n-k;j++){
-            sum += arr[j];
-            curAvg = sum/(j-i+1);
-            double cost = curAvg + solve(arr,k,j+1,dp);
-            maxSum = max(cost , maxSum);
-        }
-
-        return dp[i][k] = maxSum;
-    }
     double largestSumOfAverages(vector<int>& arr, int k) {
         int n = arr.size();
         vector<vector<double>>dp(n+1,vector<double>(k+1,-1e9));
@@ -54,6 +26,5 @@ public:
             }
         }
         return dp[0][k];            
-        // return solve(arr,k,0,dp);
     }
 };
