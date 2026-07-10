9e179e55f2d434145419a9faeaeba94e8c436611
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-07-11 01:14:23 +0530

    Time: 3 ms (83.31%), Space: 11.2 MB (34.46%) - LeetHub

--- 0813-largest-sum-of-averages/0813-largest-sum-of-averages.cpp
+++ 0813-largest-sum-of-averages/0813-largest-sum-of-averages.cpp
@@ -28,9 +28,32 @@ public:
 
         return dp[i][k] = maxSum;
     }
-    double largestSumOfAverages(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<vector<double>>dp(n+1,vector<double>(k,-1));
-        return solve(nums,k,0,dp);
+    double largestSumOfAverages(vector<int>& arr, int k) {
+        int n = arr.size();
+        vector<vector<double>>dp(n+1,vector<double>(k+1,-1e9));
+        for(int i=0;i<n;i++){
+            double curAvg = arr[i];
+            for(int j=i+1; j<n ;j++){
+                curAvg += (double)arr[j];
+            }
+            curAvg = curAvg/(n-i);
+            dp[i][1] = curAvg;
+        }
+        for(int c = 2 ; c <=k  ; c++){
+            for(int i=n-1; i>=0 ; i--){
+                double curAvg = arr[i];
+                double sum = 0;
+                double maxSum = -1e9;
+                for(int j=i;j<n;j++){
+                    sum += arr[j];
+                    curAvg = sum/(j-i+1);
+                    double cost = curAvg + dp[j+1][c-1];
+                    maxSum = max(cost , maxSum);
+                }
+                dp[i][c] = maxSum;
+            }
+        }
+        return dp[0][k];            
+        // return solve(arr,k,0,dp);
     }
 };
