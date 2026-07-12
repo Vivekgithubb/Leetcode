b418499bbfed52276cdaa45179e7a81ca3dafdc8
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-07-12 11:59:45 +0530

    Time: 13 ms (99.72%), Space: 37.6 MB (97.48%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        map<int,int>mp;
-        set<int>st;
-        priority_queue<int>pq;
-        vector<int>org = arr;
-        sort(arr.begin(),arr.end());
         int n = arr.size();
 
-        for(auto i : arr)
-            st.insert(i);
-
-        int ranks = st.size();
-        for(auto s : st)
-            pq.push(s);
-
-        while(!pq.empty()){
-            int el = pq.top();
-            pq.pop();
-            mp[el] = ranks;
-            ranks--;
-        }
+        vector<pair<int,int>>v(n);
+        vector<int>rank(n);
 
         for(int i=0;i<n;i++){
-            cout<<arr[i]<<"-"<<mp[arr[i]]<<endl;
-            int rank = mp[org[i]];
-            org[i] = rank; 
+            v[i] = {arr[i],i};
+        }
+        sort(v.begin(),v.end());
+
+        int r = 1;
+        for(int i=0 ;i<n;i++){
+            if(i>0 && v[i].first != v[i-1].first){
+                r++;
+            }
+            int index = v[i].second;
+            rank[index] = r;
         }
 
-        return org;
+        return rank;
     }
 };
