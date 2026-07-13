d367d8dfdaba5518350466040c955e00bdaaf105
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-07-14 01:56:22 +0530

    Time: 22 ms (66.14%), Space: 149.2 MB (29%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -19,18 +19,9 @@ public:
                 pre[i][j] = sum;
             }
         }
-        for (const auto &row : pre) {
-            for (int x : row) {
-                cout << x << " ";
-            }
-            cout << '\n';
-        }
     }
     
     int sumRegion(int r1, int c1, int r2, int c2) {
-        cout << "pre[" << r2 << "][" << c2 << "] = " << pre[r2][c2] << '\n';
-        cout << "pre[" << r2 << "][" << c1 << "] = " << pre[r2][c1] << '\n';
-        cout << "pre[" << r1 << "][" << c2 << "] = " << pre[r1][c2] << '\n';
         int ans = pre[r2][c2];
         if( c1 > 0)
             ans -= pre[r2][c1-1];
@@ -38,6 +29,7 @@ public:
             ans -= pre[r1-1][c2];
         if(r1 > 0 && c1 > 0)
             ans += pre[r1-1][c1-1];
+
         return ans;
     }
 };
