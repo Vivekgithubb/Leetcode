70277b91102327aba8731f685b1f7dde6855e43a
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-07-13 15:02:01 +0530

    Time: 0 ms (100%), Space: 8.4 MB (48.96%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
-        vector<int>v = {1,2,3,4,5,6,7,8,9};
-        int n = v.size();
+
         string l = to_string(low);
         string h = to_string(high);
-
-        vector<int>ans;
         int lsize = l.length();
         int hsize = h.length();
-        for(int i=lsize; i<= hsize ; i++){
-            int k = 0;
-            int j = 0;
-            string str = "";
-            while( k+i <= 9){
-                while( (j-k) < i && j <= 8){
-                    str +=(v[j]+'0');
-                    j++;
-                }
-                int num = stoi(str);
-                cout<<num<<"\n";
-                if(num > high ) return ans;
-                if(num >= low && num <= high)
-                    ans.push_back(num);
-                
-                str.erase(0,1);
-                k++;
-            }
-        }
 
+        string v = "123456789";
+        vector<int>ans;
+        for(int i=lsize; i<= hsize ; i++){
+            for(int start = 0 ; start + i <=9 ; start++){
+                int num = stoi(v.substr(start,i));
+                if(num <= high && num >= low)
+                    ans.push_back(num);
+            }
+            
+        }
         return ans;
     }
 };
