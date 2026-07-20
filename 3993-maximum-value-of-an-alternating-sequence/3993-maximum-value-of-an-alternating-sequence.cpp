3555bf0628fbd772934418d8f341c22f7b1d9615
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-07-20 16:11:55 +0530

    Time: 0 ms (100%), Space: 9 MB (87.5%) - LeetHub

--- 3993-maximum-value-of-an-alternating-sequence/3993-maximum-value-of-an-alternating-sequence.cpp
+++ 3993-maximum-value-of-an-alternating-sequence/3993-maximum-value-of-an-alternating-sequence.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     long long maximumValue(int n, int s, int m) {
-        // if( n%2 == 0)
-        //     return 1LL*(s + ((1LL*n/2) * m) - 1LL*(n/2 - 1));
         if( n == 1) return s;
-        return 1LL*(s + ((1LL*n/2) * m) - 1LL*(n/2 - 1));
+
+        long long increase = 1LL*(n/2) * m;
+        long long decrease = n/2 - 1;
+
+        return 1LL*(s + increase - decrease);
+
+        // return 1LL*(s + ((1LL*n/2) * m) - 1LL*(n/2 - 1));
     }
 };
