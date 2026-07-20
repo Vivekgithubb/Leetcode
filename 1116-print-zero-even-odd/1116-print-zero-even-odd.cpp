425c4f94ef79a14f1489f116f8a88c346f199d71
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-07-21 01:33:18 +0530

    Time: 3 ms (70.77%), Space: 10.8 MB (94.04%) - LeetHub

---
+++ 1116-print-zero-even-odd/1116-print-zero-even-odd.cpp
@@ -0,0 +1,60 @@
+class ZeroEvenOdd {
+private:
+    int n;
+    int turn;
+    int count;
+    mutex m;
+    condition_variable cv;
+public:
+    ZeroEvenOdd(int n) {
+        this->n = n;
+        this->turn = 0;
+        this->count = 1;
+    }
+
+    // printNumber(x) outputs "x", where x is an integer.
+    void zero(function<void(int)> printNumber) {
+        unique_lock<mutex>lock(m);
+        while(count <= n){  
+            while(turn != 0 && count <= n){
+                cv.wait(lock);
+            }
+            if(count <= n){
+                printNumber(0);
+                turn = (count % 2 == 0 ? 1 : 2);
+            }            
+
+            cv.notify_all();
+        }
+    }
+
+    void even(function<void(int)> printNumber) {
+        unique_lock<mutex>lock(m);
+        while(count <= n){  
+            while(turn != 1 && count <= n){
+                cv.wait(lock);
+            }
+            if(count <= n){
+                printNumber(count);
+                count++;
+                turn = 0;
+            }
+            cv.notify_all();
+        }
+    }
+
+    void odd(function<void(int)> printNumber) {
+        unique_lock<mutex>lock(m);
+        while(count <= n){  
+            while(turn != 2 && count <= n){
+                cv.wait(lock);
+            }
+            if(count <= n){
+                printNumber(count);
+                count++;
+                turn = 0;
+            }
+            cv.notify_all();
+        }        
+    }
+};
