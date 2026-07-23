cd2115405a16d8873f1e233d93d59aef9f7c1f97
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-07-23 18:13:36 +0530

    Time: 3 ms (92.35%), Space: 16.6 MB (72.95%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -16,7 +16,18 @@ public:
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        for(int i=0;i<=n;i++)    dp[i][m] = 0;
+        for(int i=0;i<=m;i++)    dp[n][i] = 0;
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1; j>=0 ; j--){
+                if( nums1[i] == nums2[j])
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                else
+                    dp[i][j] = max(dp[i+1][j] , dp[i][j+1]);
+            }
+        }
         return solve(0,0,nums1,nums2,dp);
     }
 };
