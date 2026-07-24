9f0ed5cb93ca8036309d89326d6e01aa2b13de69
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-07-24 12:13:53 +0530

    Time: 0 ms (100%), Space: 11.1 MB (32.44%) - LeetHub

--- 1116-print-zero-even-odd/1116-print-zero-even-odd.cpp
+++ 1116-print-zero-even-odd/1116-print-zero-even-odd.cpp
@@ -1,40 +1,38 @@
 class ZeroEvenOdd {
 private:
     int n;
-    int turn;
-    int count;
     mutex m;
     condition_variable cv;
+    int count;
+    int turn;
 public:
     ZeroEvenOdd(int n) {
         this->n = n;
-        this->turn = 0;
         this->count = 1;
+        this->turn = 0;
     }
 
     // printNumber(x) outputs "x", where x is an integer.
     void zero(function<void(int)> printNumber) {
-        while(count <= n){  
-            unique_lock<mutex>lock(m);
-            while(turn != 0 && count <= n){
+        while(count<= n){
+            unique_lock<mutex> lock(m);
+            while(count <= n && turn != 0)
                 cv.wait(lock);
-            }
+            
             if(count <= n){
                 printNumber(0);
-                turn = (count % 2 == 0 ? 1 : 2);
-            }            
-
+                turn = (count %2 == 0 ? 1 : 2);
+            }
             cv.notify_all();
         }
     }
 
     void even(function<void(int)> printNumber) {
-        while(count <= n){  
-            unique_lock<mutex>lock(m);
-            while(turn != 1 && count <= n){
+        while(count <=n){
+            unique_lock<mutex> lock(m);
+            while(count <= n && turn != 1)
                 cv.wait(lock);
-            }
-            if(count <= n){
+            if(count <=n){
                 printNumber(count);
                 count++;
                 turn = 0;
@@ -44,17 +42,16 @@ public:
     }
 
     void odd(function<void(int)> printNumber) {
-        while(count <= n){  
-            unique_lock<mutex>lock(m);
-            while(turn != 2 && count <= n){
+        while(count <= n){
+            unique_lock<mutex> lock(m);
+            while(count <= n && turn != 2 )
                 cv.wait(lock);
-            }
-            if(count <= n){
+            if(count <=n){
                 printNumber(count);
                 count++;
                 turn = 0;
             }
-            cv.notify_all();
-        }        
+            cv.notify_all();         
+        }
     }
 };
