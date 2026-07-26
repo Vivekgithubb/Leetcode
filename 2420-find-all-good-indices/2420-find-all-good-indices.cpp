b1c964de9eb26e87208f845f1458e276665861fe
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-07-26 19:27:58 +0530

    Time: 2537 ms (5.22%), Space: 93.6 MB (45.51%) - LeetHub

---
+++ 2420-find-all-good-indices/2420-find-all-good-indices.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> goodIndices(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int>pre(n,0);
+        vector<int>suf(n,0);
+        int count = 0;
+        for(int i=1;i<n;i++){
+            pre[i] = ++count;
+            if(nums[i] > nums[i-1])
+                count = 0;
+        }
+        count = 0;
+        for(int i=n-2;i>=0;i--){
+            suf[i] = ++count;
+            if(nums[i] > nums[i+1])
+                count = 0;   
+        }   
+        for(auto i : pre)
+            cout<<i<<"\t";
+        cout<<endl;
+        for(auto i : suf)
+            cout<<i<<"\t";
+        vector<int>ans;
+        for(int i=k;i <= n-k ;i++){
+            if(pre[i] >= k && suf[i] >= k)
+                ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
