6c478419dc71d41cb9c85f3d270b42d52b5c6a87
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-07-27 01:15:50 +0530

    Time: 0 ms (100%), Space: 22.4 MB (32.2%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
@@ -4,28 +4,22 @@ public:
         int n = nums.size();
         int maxL = 0;
         for(int i=0;i<n-1;i++){
-            int count = 0;
+            int count = 1;
             bool t1=false,t2=false;
             if(i < n-1 && nums[i] >= nums[i+1]) continue;
             int j = i;
-            while( j< n-1 && nums[j] < nums[j+1]){
+            while( j < n-1 && nums[j] < nums[j+1]){
                 t1 = true;
                 count+=1;
                 j++;
             }
-            count++;
-            j++;
-            while( j < n && nums[j-1] > nums[j]){
+            while( j < n-1 && nums[j] > nums[j+1]){
                 t2 = true;
                 count+=1;
                 j++;
             }
-            if(t1 && t2){
-                if(maxL < count){
-                    maxL = count;
-                    cout<<j;
-                }
-            }
+            if(t1 && t2) maxL = max(maxL,count);
+            i = j-1;
         }
 
         return maxL;
