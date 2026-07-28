521f18d5a36d115b9e9f55c39355c99f1c245f53
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-07-28 13:59:57 +0530

    Time: 23 ms (82.86%), Space: 79.8 MB (16.92%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -5,20 +5,22 @@ public:
         string str2="";
         string str3="";
         int n = s.length();
-        map<char,int>mp;
+        vector<int>mp(26,0);
         for(auto c : s){
-            mp[c]++;
+            mp[c-'a']++;
         }
-        for(auto &[c,freq] : mp){
+        for(int i=0;i<26;i++){
+            int freq = mp[i];
             if(freq >= 2){
-                str1.append(freq/2, c);
-                str2.append(freq/2, c);
-                freq = freq % 2 == 0 ? 0 : 1;
+                str1.append(freq/2, i + 'a');
+                str2.append(freq/2, i + 'a');
+                mp[i] = mp[i] % 2 == 0 ? 0 : 1;
             }
         }
         if( n%2 != 0){
-            for(auto &[c,freq] : mp){
-                str3.append(freq,c);
+            for(int i=0;i<26;i++){
+                int freq = mp[i];
+                str3.append(freq, i + 'a');
             }
         }
         reverse(str2.begin(),str2.end());
