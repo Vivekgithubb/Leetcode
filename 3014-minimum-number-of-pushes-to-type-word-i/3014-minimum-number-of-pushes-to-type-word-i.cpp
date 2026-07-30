a73dd0aaa9d39d80bcfca05d9952e88b4d4d85f1
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-07-30 12:06:52 +0530

    Time: 0 ms (100%), Space: 8.3 MB (77.73%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -2,17 +2,11 @@ class Solution {
 public:
     int minimumPushes(string word) {
         int n = word.size();
-        unordered_set<char>st;
-        for(auto s : word)
-            st.insert(s);
-        int req = st.size();
-        if(req <= 8 ) return req;
-        int press = req / 8;
-        int spare = req % 8;
-        if( press == 3)
-            return (6 *8) + (spare * (press + 1 ));
-        if( press == 2)
-            return (3 * 8) + (spare * (press + 1 ));
+        if(n <= 8 ) return n;
+        int press = n / 8;
+        int spare = n % 8;
+        if( press == 3) return (6*8) + (spare * (press + 1 ));
+        if( press == 2) return (3*8) + (spare * (press + 1 ));
         return (8) + (spare * (press + 1 ));
     }
 };
