6ddf52247dad07377c112f18a2d66f410037e67d
D Vivek Pai <157715231+Vivekgithubb@users.noreply.github.com>
2026-08-17 21:12:55 +0530

    Time: 2448 ms (28.57%), Space: 178.6 MB (100%) - LeetHub

---
+++ 4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights/4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minPenalty(int p, vector<int>& l, vector<int>& at) {
+        int ans = INT_MIN;
+        int cur = INT_MAX;
+        int maxE = *max_element(l.begin(),l.end());
+        for(int i=0;i<at.size();i++){
+            int r = at[i]%p;
+            int wait = -1;
+
+            if( r < maxE)
+                wait = 0;
+            else 
+                wait = p - r;
+
+            cout<<wait<<endl;
+            ans = max(wait,ans);
+        }
+        return ans;
+    }
+};
